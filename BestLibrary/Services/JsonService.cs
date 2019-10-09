4fa91eb2afa8b67cc8d1e54aa6288a51346e75be
Mike Lowe <mikelowe983@gmail.com>
2019-10-09 15:29:06 -0300

    Add project

---
+++ BestLibrary/Services/JsonService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BestLibrary.Services
+{
+    public class JsonService
+    {        
+        public static string Serialize(object toSerialize)
+        {
+            return JsonConvert.SerializeObject(toSerialize);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
